2d6c6f8b9ff814efd264e12519c639383e35334c
Kongtup <ttansiri524@gmail.com>
2025-07-22 10:39:26 +0700

    fix: terms url change tab

--- template/terms.php
+++ template/terms.php
@@ -6,120 +6,98 @@
     <section data-section="terms">
         <div class="sc-inner sc-inner--follows sc-inner--bottom-extra">
             <div class="container">
-                <div class="tab-container">
-                    <div class="sc-ttl animate fadeIn">
-                        <h1 class="size-h1 uppercase">Privacy & Terms</h1>
-                    </div>
+                <div class="sc-ttl animate fadeIn">
+                    <h1 class="size-h1 uppercase">Privacy & Terms</h1>
+                </div>
 
-                    <div class="tab-links animate fadeIn">
-                        <div class="swiper auto centered-m click">
-                            <div class="swiper-wrapper">
-                                <div class="swiper-slide">
-                                    <div class="tab clickable">
-                                        <a href="#terms" class="active">Terms & Conditions</a>
-                                    </div>
+                <div class="tab-links animate fadeIn">
+                    <div class="swiper auto centered-m click">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="tab clickable">
+                                    <a href="<?php echo $root; ?>terms.php" class="active">Terms & Conditions</a>
                                 </div>
+                            </div>
 
-                                <div class="swiper-slide">
-                                    <div class="tab clickable">
-                                        <a href="#privacy">Privacy Policy</a>
-                                    </div>
+                            <div class="swiper-slide">
+                                <div class="tab clickable">
+                                    <a href="<?php echo $root; ?>terms.php">Privacy Policy</a>
                                 </div>
+                            </div>
 
-                                <div class="swiper-slide">
-                                    <div class="tab clickable">
-                                        <a href="#cookies">COOKIES</a>
-                                    </div>
+                            <div class="swiper-slide">
+                                <div class="tab clickable">
+                                    <a href="<?php echo $root; ?>terms.php">COOKIES</a>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="tab-contents">
-                        <div class="tab-content active" id="terms">
-                            <div class="content-ttl">
-                                <h2>Terms & Conditions</h2>
-                            </div>
+                <div class="content">
+                    <div class="content-ttl">
+                        <h2>Terms & Conditions</h2>
+                    </div>
 
-                            <div class="entry-content entry-fadeIn">
-                                <h3>Welcome to ARAY!</h3>
-                                <p>These terms and conditions outline the rules and regulations for the use of Company Name's Website, located at Website.com.
-                                    By accessing this website we assume you accept these terms and conditions. Do not continue to use Website Name if you do not agree to take all of the terms and conditions stated on this page.</p>
+                    <div class="entry-content entry-fadeIn">
+                        <h3>Welcome to ARAY!</h3>
+                        <p>These terms and conditions outline the rules and regulations for the use of Company Name's Website, located at Website.com.
+                            By accessing this website we assume you accept these terms and conditions. Do not continue to use Website Name if you do not agree to take all of the terms and conditions stated on this page.</p>
 
-                                <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: “Client”, “You” and “Your” refers to you, the person log on this website and compliant to the Company's terms and conditions. “The Company”, “Ourselves”, “We”, “Our” and “Us”, refers to our Company. “Party”, “Parties”, or “Us”, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+                        <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: “Client”, “You” and “Your” refers to you, the person log on this website and compliant to the Company's terms and conditions. “The Company”, “Ourselves”, “We”, “Our” and “Us”, refers to our Company. “Party”, “Parties”, or “Us”, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
 
-                                <h3>Cookies</h3>
-                                <p>We employ the use of cookies. By accessing Website Name, you agreed to use cookies in agreement with the Company Name's </p>
+                        <h3>Cookies</h3>
+                        <p>We employ the use of cookies. By accessing Website Name, you agreed to use cookies in agreement with the Company Name's </p>
 
-                                <h3>Privacy Policy.</h3>
-                                <p>Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
+                        <h3>Privacy Policy.</h3>
+                        <p>Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
 
-                                <h3>License</h3>
-                                <p>Unless otherwise stated, Company Name and/or its licensors own the intellectual property rights for all material on Website Name. All intellectual property rights are reserved. You may access this from Website Name for your own personal use subjected to restrictions set in these terms and conditions.</p>
+                        <h3>License</h3>
+                        <p>Unless otherwise stated, Company Name and/or its licensors own the intellectual property rights for all material on Website Name. All intellectual property rights are reserved. You may access this from Website Name for your own personal use subjected to restrictions set in these terms and conditions.</p>
 
-                                <p>You must not:</p>
-                                <ul class="wp-block-list">
-                                    <li>Republish material from this website without permission.</li>
-                                    <li>Sell, rent or sub-license material from the website;</li>
-                                    <li>Reproduce, duplicate or copy material from this website.</li>
-                                    <li>Redistribute content from this website - except for content specifically and expressly made available for redistribution.</li>
-                                </ul>
+                        <p>You must not:</p>
+                        <ul class="wp-block-list">
+                            <li>Republish material from this website without permission.</li>
+                            <li>Sell, rent or sub-license material from the website;</li>
+                            <li>Reproduce, duplicate or copy material from this website.</li>
+                            <li>Redistribute content from this website - except for content specifically and expressly made available for redistribution.</li>
+                        </ul>
 
-                                <p>
-                                    This Agreement shall begin on the date hereof.
-                                    <br>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Company Name does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Company Name,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, Company Name shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.
-                                </p>
+                        <p>
+                            This Agreement shall begin on the date hereof.
+                            <br>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Company Name does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Company Name,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, Company Name shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.
+                        </p>
 
-                                <figure class="wp-block-image size-large is-resized">
-                                    <img src="./assets/media/design/map.jpg" alt="" style="width: 490px; height: auto;">
-                                </figure>
+                        <figure class="wp-block-image size-large is-resized">
+                            <img src="./assets/media/design/map.jpg" alt="" style="width: 490px; height: auto;">
+                        </figure>
 
-                                <p>
-                                    Company Name reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.
-                                </p>
+                        <p>
+                            Company Name reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.
+                        </p>
 
-                                <p>You warrant and represent that:</p>
-                                <ul class="wp-block-list">
-                                    <li>You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;</li>
-                                    <li>The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
-                                    <li>The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy;</li>
-                                    <li>The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
-                                </ul>
+                        <p>You warrant and represent that:</p>
+                        <ul class="wp-block-list">
+                            <li>You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;</li>
+                            <li>The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
+                            <li>The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy;</li>
+                            <li>The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
+                        </ul>
 
-                                <p>
-                                    You hereby grant Company Name a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.
-                                </p>
+                        <p>
+                            You hereby grant Company Name a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.
+                        </p>
 
-                                <h3>
-                                    Hyperlinking to our Content
-                                </h3>
-                                <p>The following organizations may link to our Website without prior written approval:</p>
-                                <ul class="wp-block-list">
-                                    <li>Government agencies;</li>
-                                    <li>Search engines;</li>
-                                    <li>News organizations;</li>
-                                    <li>Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and</li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="tab-content" id="privacy">
-                            <div class="content-ttl">
-                                <h2>Privacy Policy</h2>
-                            </div>
-
-                            <div class="entry-content entry-fadeIn">
-                            </div>
-                        </div>
-
-                        <div class="tab-content" id="cookies">
-                            <div class="content-ttl">
-                                <h2>Cookies</h2>
-                            </div>
-
-                            <div class="entry-content entry-fadeIn">
-                            </div>
-                        </div>
+                        <h3>
+                            Hyperlinking to our Content
+                        </h3>
+                        <p>The following organizations may link to our Website without prior written approval:</p>
+                        <ul class="wp-block-list">
+                            <li>Government agencies;</li>
+                            <li>Search engines;</li>
+                            <li>News organizations;</li>
+                            <li>Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and</li>
+                        </ul>
                     </div>
                 </div>
             </div>
